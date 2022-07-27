bdd1c510709b296e16e6e3a01b66c15ec6d7003e
IT <akinlabisamon15@gmail.com>
2022-07-27 21:54:59 +0100

    28/07/2022

--- app/Http/Controllers/VertualController.php
+++ app/Http/Controllers/VertualController.php
@@ -36,7 +36,7 @@ class VertualController
                 CURLOPT_SSL_VERIFYHOST => 0,
                 CURLOPT_SSL_VERIFYPEER => 0,
                 CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS => array('account_name' => $user->username, 'business_short_name' => 'YELLOWMANTELECOMS', 'uniqueid' => $user->username, 'email' => $user->email, 'phone' => '08146328645', 'webhook_url' => 'https://yellowmantelecoms.com.ng//api/run',),
+                CURLOPT_POSTFIELDS => array('account_name' => $user->username, 'business_short_name' => 'YELLOWMANTELECOMS', 'uniqueid' => $user->username, 'email' => $user->email, 'phone' => '08146328645', 'webhook_url' => 'https://yellowmantelecoms.com.ng/api/run',),
                 CURLOPT_HTTPHEADER => array(
                     'Authorization: mcd_key_75rq4][oyfu545eyuriup1q2yue4poxe3jfd'
                 ),
